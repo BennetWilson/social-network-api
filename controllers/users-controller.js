2f39e06426656e9522b663ee2265cfe246f14b80
Bennet Wilson <bennetwilson@gmail.com>
2022-02-08 15:39:47 -0700

    filling out thoughts controller.

---
+++ controllers/users-controller.js
@@ -0,0 +1,59 @@
+const { Users } = require('../models');
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+
+    .populate({
+      path: 'thoughts', 
+      select: '-v'
+    })
+    .populate({
+      path: 'friends', 
+      select: '-v'
+    })
+    .select('-v')
+
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+
+    .populate({
+      path: 'thoughts', 
+      select: '-v'
+    })
+    .populate({
+      path: 'friends', 
+      select: '-v'
+    })
+      .select('-v')
+
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a user and associated apps
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user) 
+      )
+      .then(() => res.json({ message: 'User and associated thougths deleted!'}))
+      .catch((err) => res.status(500).json(err));
+  },
+};
